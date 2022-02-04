56964cb8899f8886ddc963d2fbaceb164749b7df
mynameisvinn <vtang@etsy.com>
2022-02-04 13:31:50 -0500

    include constructor

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# Tontine
-A toy example of a tontine written in solidity. 
-
-## Concepts
-The main concepts that are examined in this example are:
-
-* working with fractions (Solidity cannot handle floating point numbers)
-* global state (for me, global variables are not intuitive)
+# Edge & Node Challenge
+A solution to [Edge & Node challenge](https://github.com/edgeandnode/contracts-challenge/tree/exercise-4).
--- tontine.sol
+++ tontine.sol
@@ -1,15 +1,15 @@
 pragma solidity >=0.5.0 <0.7.0;
 
 contract Tontine {
-    address public owner;
+    address public owner;  // ethpool team
     uint public pool; 
     uint public total_shares; 
     uint private share; 
     mapping(address => uint) public balances;
     address[] public addressIndices;
 
-    function Start() public{
-        owner = msg.sender;
+    constructor() public {
+         owner = msg.sender;
     }
     
     function depositReward() payable public {
@@ -20,6 +20,7 @@ contract Tontine {
     }
     
     function depositPool() payable public{ 
+        require(msg.value >= 0, "Positive deposits, only.");
         addressIndices.push(msg.sender);
         balances[msg.sender] += msg.value;
         pool += msg.value;
@@ -27,6 +28,8 @@ contract Tontine {
     
 
     function withdrawPool() payable public {
+        require(balances[msg.sender] > 0, "Player is not a participant.");
+        require(pool > 0, "Not enough money in the pool.");
 
         uint arrayLength = addressIndices.length;
 
@@ -34,11 +37,12 @@ contract Tontine {
             total_shares += balances[addressIndices[i]];
         }
 
-        share = (balances[msg.sender] * pool * 100000) / (total_shares  * 100000);
+        share = (balances[msg.sender] * pool * 1000000) / (total_shares  * 1000000);
         pool -= share;
 
         balances[msg.sender] = 0;
         total_shares = 0;
+
         msg.sender.transfer(share);
     }
 }
